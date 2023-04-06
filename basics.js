93f87d96558dfbf2593304e59ac829c85046041e
Arpit <arpitbansal422@gmail.com>
2023-04-06 20:57:20 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,20 @@
+//console.log("hi")  // to print at console
+//alert("hi") // to give alert to user
+// var ans = prompt("What is your name?");
+// console.log("Hi " + ans);
+// alert("Hi "+ ans);
+var ans = prompt("What is your name?");
+var str = 10;
+if(ans == "Arpit"){
+    alert("Welcome");
+}
+else if( ans == "Roy"){
+    alert("Welcome")
+}
+else
+alert("Bye");
+if(str =="10"){
+    console.log(str)
+}
+if(str==10)
+console.log(str+str);
